e8cdcd4265d78b7abaaacece1069426766bba543
Jackson Ho <mintri1199@gmail.com>
2019-04-17 20:06:39 -0700

    [Modified] Fixed bugs of unable to load model

--- MOB_2.1_Tutorial_Starter_App/loaner/controllers/ViewController.swift
+++ MOB_2.1_Tutorial_Starter_App/loaner/controllers/ViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
     var items: [Item] = []
+    var store: ItemStore!
     
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -25,10 +27,40 @@ class ViewController: UIViewController {
         flow.itemSize = CGSize(width: screenSize.width / 2 - horizontalPadding * 2, height: screenSize.width / 2 - verticalPadding * 2)
         flow.sectionInset = UIEdgeInsets(top: verticalPadding, left: horizontalPadding, bottom: verticalPadding, right: horizontalPadding)
         collectionView.collectionViewLayout = flow
+        
+        updateDataSource()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Save the new items in the Managed Object Context
+        store.saveContext()
+        updateDataSource()
+    }
+    
     func createNewItem() -> Item {
-        return Item(itemTitle: "Untitled Item")
+        let context = store.persistentContainer.viewContext
+        let newItem = NSEntityDescription.insertNewObject(forEntityName: "Item", into: context) as! Item
+        
+        return newItem
+    }
+    
+    // populate an array with fetched results on success, or to delete all items from that array on failure
+    private func updateDataSource() {
+        self.store.fetchPersistedData {
+            
+            (fetchItemsResult) in
+            
+            switch fetchItemsResult {
+            case let .success(items):
+                self.items = items
+            case .failure(_):
+                self.items.removeAll()
+            }
+            // reload the collection view's data source to present the current data set to the user
+            self.collectionView.reloadSections(IndexSet(integer: 0))
+        }
     }
     
     func add(saved item: Item) {
@@ -73,7 +105,7 @@ class ViewController: UIViewController {
             }
         }
     }
-      
+    
     @IBAction func unwindToHome(_ segue: UIStoryboardSegue) {
         guard let identifier = segue.identifier else {
             return
